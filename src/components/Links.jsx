a69a565b50650c51eaeca9188a7840427ed44ffd
陳曦 <eriksonchen@gmail.com>
2025-08-29 17:50:31 +0800

    optimize link position

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import linkList from 'utilities/bookmarkLink.jsx';
 import 'components/Links.css';
 
@@ -9,12 +9,32 @@ export default function Links({show}) {
                 <i className="fa-solid fa-bookmark"/>
             </div>
             <div className="filler"/>
-            {linkList.map(link => <>
-                <div className="link-wrap">
+            {linkList.map((link, i) => <>
+                <div className="link-header">
                     <i className={'fa-solid fa-' + link.icon}/>
                     <a>{link.class}</a>
                 </div>
-                <div className="sub-links" style={{'--padding': 1 + 'rem'}}>
+                <div 
+                    className="sub-links" 
+                    style={{
+                        '--padding': 
+                            (headerIndex => {
+                                const windowHeight = window.innerHeight;
+                                const remToPx = 16;
+                                const linkHeight = 3.4 * remToPx; // rem to px
+                                const headerPosition = windowHeight / 2 + (headerIndex + 1 - linkList.length / 2 - .5) * linkHeight;
+                                const linksHeight = link.child.length * linkHeight;
+                                let padding;
+                                if (headerPosition + linksHeight / 2 <= windowHeight - remToPx) 
+                                    padding = headerPosition - linksHeight / 2;
+                                else 
+                                    padding = windowHeight - linksHeight - remToPx;
+                                if (padding < remToPx) 
+                                    padding = remToPx;
+                                return padding + 'px';
+                            })(i)
+                    }}
+                >
                     <div className="filler"/>
                     {link.child.map(_link =>
                         <a id={_link}>{_link}</a>
