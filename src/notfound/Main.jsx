339abdda70e0b715fa8ebec22f767e04f4b84a3f
陳曦 <eriksonchen@gmail.com>
2025-10-11 21:34:26 +0800

    Automatic page builds

--- src/notfound/Main.jsx
+++ src/notfound/Main.jsx
@@ -9,7 +9,8 @@ export default function Main() {
         <main className='notfound'> 
             <h1>404</h1>
             <h2>this page could not be found.</h2>
-            <p>Maybe you should <a href='https://hsiii.github.io/pages/portfolio/'>check out my portfolio</a> instead.</p>
+            {/* <p>Maybe you should <a href='https://hsiii.github.io/pages/portfolio/'>check out my portfolio</a> instead.</p> */}
+            <p>Maybe you should <a href='https://hsiii.github.io'>check out my other pages</a> instead.</p>
         </main>
     </>
 }
